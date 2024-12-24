984302a664b982f5ffe2418dd8223c84f58d9ad8
nitin <gniitin260@gmail.com>
2024-12-25 01:59:34 +0530

    Update docker-build.sh

---
+++ docker-build.sh
@@ -0,0 +1,4 @@
+docker build -t adservice:v1.0 src/adservice/.
+docker build -t cartservice:v1.0 src/cartservice/.
+docker build -t checkoutservice:v1.0 src/checkoutservice/.
+docker build -t currencyservice:v1.0 src/currencyservice/.

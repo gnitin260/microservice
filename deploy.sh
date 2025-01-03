f5ca1372a5ed529c8f9cd589d894659c925e9092
nitin <gniitin260@gmail.com>
2025-01-04 02:55:02 +0530

    deploy

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+kubectl create namespace devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/frontend/deployment.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/frontend/service.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/frontend/serviceaccount.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/frontend/configMap.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/currencyservice/deployment.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/currencyservice/service.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/currencyservice/serviceaccount.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/currencyservice/configMap.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/catalog/deployment.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/catalog/service.yaml -n devops
+kubectl apply -f /Users/nitin/Desktop/Devops/microservice/k8/catalog/serviceaccount.yaml -n devops
+kubectl get pods -n devops
+kubectl get services -n devops
+kubectl get serviceaccount -n devops
--- docker-build.sh
+++
@@ -1,4 +0,0 @@
-docker build -t adservice:v1.0 src/adservice/.
-docker build -t cartservice:v1.0 src/cartservice/.
-docker build -t checkoutservice:v1.0 src/checkoutservice/.
-docker build -t currencyservice:v1.0 src/currencyservice/.

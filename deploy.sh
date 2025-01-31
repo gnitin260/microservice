f9d988e08cd92e38b050c6b6e16d3636e478a2e3
nitin gautam <gnitin260@gmail.com>
2025-01-31 18:38:28 +0000

    update chart and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,51 @@
-kubectl create namespace devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/frontend/deployment.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/frontend/service.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/frontend/serviceaccount.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/frontend/configMap.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/currencyservice/deployment.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/currencyservice/service.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/currencyservice/serviceaccount.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/currencyservice/configMap.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/catalog/deployment.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/catalog/service.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/catalog/serviceaccount.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/adservice/deployment.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/adservice/service.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/adservice/serviceaccount.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/cartservice/deployment.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/cartservice/service.yaml -n devops
-kubectl apply -f /home/ubuntu/k8/microservice/k8/cartservice/serviceaccount.yaml -n devops
-kubectl get pods -n devops
-kubectl get services -n devops
-kubectl get serviceaccount -n devops
+#!/bin/bash 
+
+echo "Enter type of process"
+echo "i.e., install, update, uninstall"
+read process
+
+if [ "$process" = "install" ]; then
+    echo "------------------------------------------------"
+    helm install frontend ci-cd/chart/frontend/ -n devops
+    echo "------------------------------------------------"
+    helm install currencyservice ci-cd/chart/currencyservice/ -n devops
+    echo "------------------------------------------------"
+    helm install catalog ci-cd/chart/catalog/ -n devops
+    echo "------------------------------------------------"
+    helm install adservice ci-cd/chart/adservice/ -n devops
+    echo "------------------------------------------------"
+elif [ "$process" = "update" ]; then
+    echo "------------------------------------------------"
+    helm upgrade frontend ci-cd/chart/frontend/ --values ci-cd/chart/frontend/values.yaml -n devops
+    echo "------------------------------------------------"
+    helm upgrade currencyservice ci-cd/chart/currencyservice/ --values ci-cd/chart/currencyservice/values.yaml -n devops
+    echo "------------------------------------------------"
+    helm upgrade catalog ci-cd/chart/catalog/ --values ci-cd/chart/catalog/values.yaml -n devops
+    echo "------------------------------------------------"
+    helm upgrade adservice ci-cd/chart/adservice/ --values ci-cd/chart/adservice/values.yaml -n devops
+    echo "------------------------------------------------"
+elif [ "$process" = "uninstall" ]; then
+    echo "------------------------------------------------"
+    helm uninstall frontend -n devops
+    echo "------------------------------------------------"
+    helm uninstall currencyservice -n devops
+    echo "------------------------------------------------"
+    helm uninstall catalog -n devops
+    echo "------------------------------------------------"
+    helm uninstall adservice -n devops
+    echo "------------------------------------------------"
+else
+    echo "Bad input: $process. Please use 'install', 'update', or 'uninstall'."
+fi
+
+if [ "$process" = "install" ] || [ "$process" = "update" ] || [ "$process" = "uninstall" ]; then
+    echo "------------------------------------------------"
+    kubectl get pods -n devops
+    echo "------------------------------------------------"
+    kubectl get services -n devops
+    echo "------------------------------------------------"
+    kubectl get serviceaccount -n devops
+    echo "------------------------------------------------"
+else    
+    echo ""
+fi
